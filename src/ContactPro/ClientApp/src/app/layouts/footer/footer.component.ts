6ae55428cffa7fb6fad74587881d066df6d6b3cf
Daniel Ho <webdevhod@gmail.com>
2023-05-17 18:40:11 -0700

    Added header links and footer icons and links.

--- src/ContactPro/ClientApp/src/app/layouts/footer/footer.component.ts
+++ src/ContactPro/ClientApp/src/app/layouts/footer/footer.component.ts
@@ -4,4 +4,6 @@ import { Component } from "@angular/core";
   selector: "jhi-footer",
   templateUrl: "./footer.component.html",
 })
-export class FooterComponent {}
+export class FooterComponent {
+  currentYear: number = (new Date()).getFullYear();
+}
