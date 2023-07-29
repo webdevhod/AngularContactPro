b27b4f4c6c7582f6717d7bf816ac5f4f0bea676c
Daniel Ho <webdevhod@gmail.com>
2023-07-28 23:41:00 -0700

    Fixed main layout.

--- src/ContactPro/ClientApp/src/app/login/login.component.ts
+++ src/ContactPro/ClientApp/src/app/login/login.component.ts
@@ -1,9 +1,9 @@
 import {
-  Component,
-  ViewChild,
-  OnInit,
-  AfterViewInit,
-  ElementRef,
+    Component,
+    ViewChild,
+    OnInit,
+    AfterViewInit,
+    ElementRef,
 } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
@@ -12,57 +12,58 @@ import { LoginService } from "app/login/login.service";
 import { AccountService } from "app/core/auth/account.service";
 
 @Component({
-  selector: "jhi-login",
-  templateUrl: "./login.component.html",
+    selector: "jhi-login",
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit, AfterViewInit {
-  @ViewChild("username", { static: false })
-  username!: ElementRef;
+    @ViewChild("username", { static: false })
+    username!: ElementRef;
 
-  authenticationError = false;
+    authenticationError = false;
 
-  loginForm = this.fb.group({
-    username: [null, [Validators.required]],
-    password: [null, [Validators.required]],
-    rememberMe: [false],
-  });
-
-  constructor(
-    private accountService: AccountService,
-    private loginService: LoginService,
-    private router: Router,
-    private fb: FormBuilder
-  ) {}
-
-  ngOnInit(): void {
-    // if already authenticated then navigate to home page
-    this.accountService.identity().subscribe(() => {
-      if (this.accountService.isAuthenticated()) {
-        this.router.navigate([""]);
-      }
+    loginForm = this.fb.group({
+        username: [null, [Validators.required]],
+        password: [null, [Validators.required]],
+        rememberMe: [false],
     });
-  }
 
-  ngAfterViewInit(): void {
-    this.username.nativeElement.focus();
-  }
+    constructor(
+        private accountService: AccountService,
+        private loginService: LoginService,
+        private router: Router,
+        private fb: FormBuilder
+    ) {}
 
-  login(): void {
-    this.loginService
-      .login({
-        username: this.loginForm.get("username")!.value,
-        password: this.loginForm.get("password")!.value,
-        rememberMe: this.loginForm.get("rememberMe")!.value,
-      })
-      .subscribe({
-        next: () => {
-          this.authenticationError = false;
-          if (!this.router.getCurrentNavigation()) {
-            // There were no routing during login (eg from navigationToStoredUrl)
-            this.router.navigate([""]);
-          }
-        },
-        error: () => (this.authenticationError = true),
-      });
-  }
+    ngOnInit(): void {
+        // if already authenticated then navigate to home page
+        this.accountService.identity().subscribe(() => {
+            if (this.accountService.isAuthenticated()) {
+                this.router.navigate([""]);
+            }
+        });
+    }
+
+    ngAfterViewInit(): void {
+        this.username.nativeElement.focus();
+    }
+
+    login(): void {
+        this.loginService
+            .login({
+                username: this.loginForm.get("username")!.value,
+                password: this.loginForm.get("password")!.value,
+                rememberMe: this.loginForm.get("rememberMe")!.value,
+            })
+            .subscribe({
+                next: () => {
+                    this.authenticationError = false;
+                    if (!this.router.getCurrentNavigation()) {
+                        // There were no routing during login (eg from navigationToStoredUrl)
+                        this.router.navigate([""]);
+                    }
+                },
+                error: () => (this.authenticationError = true),
+            });
+    }
 }
