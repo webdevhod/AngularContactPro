5b1591933158a28f58445d01e55489344a43d6a5
Daniel Ho <webdevhod@gmail.com>
2024-03-15 18:20:17 -0700

    Export jwt type

--- src/ContactPro/ClientApp/src/app/core/auth/auth-jwt.service.ts
+++ src/ContactPro/ClientApp/src/app/core/auth/auth-jwt.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
-import { LocalStorageService, SessionStorageService } from "ngx-webstorage";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 
-import { ApplicationConfigService } from "../config/application-config.service";
-import { Login } from "app/login/login.model";
+import { ApplicationConfigService } from '../config/application-config.service';
+import { Login } from 'app/login/login.model';
 
-type JwtToken = {
+export type JwtToken = {
   id_token: string;
 };
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class AuthServerProvider {
   constructor(
     private http: HttpClient,
@@ -21,30 +21,21 @@ export class AuthServerProvider {
   ) {}
 
   getToken(): string {
-    const tokenInLocalStorage: string | null =
-      this.localStorageService.retrieve("authenticationToken");
-    const tokenInSessionStorage: string | null =
-      this.sessionStorageService.retrieve("authenticationToken");
-    return tokenInLocalStorage ?? tokenInSessionStorage ?? "";
+    const tokenInLocalStorage: string | null = this.localStorageService.retrieve('authenticationToken');
+    const tokenInSessionStorage: string | null = this.sessionStorageService.retrieve('authenticationToken');
+    return tokenInLocalStorage ?? tokenInSessionStorage ?? '';
   }
 
   login(credentials: Login): Observable<void> {
     return this.http
-      .post<JwtToken>(
-        this.applicationConfigService.getEndpointFor("api/authenticate"),
-        credentials
-      )
-      .pipe(
-        map((response) =>
-          this.authenticateSuccess(response, credentials.rememberMe)
-        )
-      );
+      .post<JwtToken>(this.applicationConfigService.getEndpointFor('api/authenticate'), credentials)
+      .pipe(map(response => this.authenticateSuccess(response, credentials.rememberMe)));
   }
 
   logout(): Observable<void> {
-    return new Observable((observer) => {
-      this.localStorageService.clear("authenticationToken");
-      this.sessionStorageService.clear("authenticationToken");
+    return new Observable(observer => {
+      this.localStorageService.clear('authenticationToken');
+      this.sessionStorageService.clear('authenticationToken');
       observer.complete();
     });
   }
@@ -52,11 +43,11 @@ export class AuthServerProvider {
   private authenticateSuccess(response: JwtToken, rememberMe: boolean): void {
     const jwt = response.id_token;
     if (rememberMe) {
-      this.localStorageService.store("authenticationToken", jwt);
-      this.sessionStorageService.clear("authenticationToken");
+      this.localStorageService.store('authenticationToken', jwt);
+      this.sessionStorageService.clear('authenticationToken');
     } else {
-      this.sessionStorageService.store("authenticationToken", jwt);
-      this.localStorageService.clear("authenticationToken");
+      this.sessionStorageService.store('authenticationToken', jwt);
+      this.localStorageService.clear('authenticationToken');
     }
   }
 }
